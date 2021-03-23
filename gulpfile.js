f36e7684408c3bff9f5b3702e85c3bccb8f72b63
midoghranek <midoghranek@hotmail.com>
2021-03-23 03:19:29 +0200

    🚀 RELEASE: intial

---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+// packages
+const { src, dest, watch, parallel, series } = require("gulp");
+const connect = require("gulp-connect");
+const webp = require("gulp-webp");
+const sass = require("gulp-sass");
+const rimraf = require("rimraf");
+sass.compiler = require("sass");
+
+// compile sass
+const compileSass = (done) => {
+  src("src/sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("src/css"))
+    .pipe(connect.reload());
+  done();
+};
+
+// compile html
+const compileHTML = (done) => {
+  src("./src/*.html").pipe(connect.reload());
+  done();
+};
+
+// webp
+const image = () => {
+  src("src/img/*.png").pipe(webp()).pipe(dest("src/img"));
+};
+
+// watch changes
+const watchTask = () => {
+  watch("src/sass/**/*.scss", compileSass);
+  watch("src/*.html", compileHTML);
+};
+
+// dev server
+const dev = () => {
+  connect.server({
+    root: ["src"],
+    port: 3000,
+    livereload: true,
+  });
+};
+
+const cleanBuild = (done) => {
+  rimraf("./build", done);
+};
+
+// build
+const build = (done) => {
+  src("src/sass/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(dest("build/css"));
+  src("./src/*.html").pipe(dest("build"));
+  src("./src/img/*.png").pipe(dest("build/img"));
+  src("./src/img/*.svg").pipe(dest("build/img"));
+  src("src/img/*.png").pipe(webp()).pipe(dest("build/img"));
+  done();
+};
+
+// production server [build]
+const start = () => {
+  connect.server({
+    root: ["build"],
+    port: 4000,
+    livereload: false,
+  });
+};
+
+// tasks
+
+// 1. gulp dev
+exports.dev = parallel(image, watchTask, dev);
+
+// 2. gulp build
+exports.build = series(cleanBuild, build);
+
+// 3. gulp start
+exports.start = start;
+
+// 4. gulp default
+exports.default = series(cleanBuild, build, start);
